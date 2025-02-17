87bf34af004aae6066a809191555f4b549687959
Shawn Conboy <conboysbusiness@gmail.com>
2025-02-16 20:12:24 -0500

    init files

---
+++ javaScript/app.js
@@ -0,0 +1,11 @@
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!"
+}
+
+function add7(num) {
+    return num + 7;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
